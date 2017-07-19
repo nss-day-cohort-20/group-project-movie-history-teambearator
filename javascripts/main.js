a018a8ef9bf0d495a2dbe5abd1126f8dd49661b9
Greta Moseley <greta.moseley@gmail.com>
2017-07-19 14:07:29 -0500

    add login and logout buttons, event listeners, and class toggling on buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,5 +6,23 @@ let userFactory = require('./user-factory.js');
 
 //event listeners
 
+$("#login").click(function() {
+	userFactory.logInGoogle()
+	//wrapped in promises automatically
+	.then((result)=>{
+		let user = result.user.uid;
+		console.log("user", user);
+ 		// movieController.loadMoviesToDom();
+ 		$('#logout').toggleClass('isHidden');
+ 		$('#login').toggleClass('isHidden');
+	});
+});
+
+//user can log out by clicking logout button and page refreshes
+$("#logout").click(function(){
+	userFactory.logOutGoogle();
+
+});
+
 
 
